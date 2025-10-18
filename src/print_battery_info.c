ccbffa1ff63755c6a2e5d8ed4da64466db64ac51
techflashYT <officialTechflashYT@gmail.com>
2025-10-17 20:45:12 -0700

    battery: don't multiply seconds by 60 to get seconds
    As the kernel docs state, this values is represented in seconds already.
    Multiplying it by 60 just comically over-inflates the estimated time, so
    don't do that.

--- src/print_battery_info.c
+++ src/print_battery_info.c
@@ -191,7 +191,7 @@ static bool slurp_battery_info(battery_info_ctx_t *ctx, struct battery_info *bat
         } else if (BEGINS_WITH(last, "POWER_SUPPLY_VOLTAGE_NOW="))
             voltage = abs(atoi(walk + 1));
         else if (BEGINS_WITH(last, "POWER_SUPPLY_TIME_TO_EMPTY_NOW="))
-            batt_info->seconds_remaining = abs(atoi(walk + 1)) * 60;
+            batt_info->seconds_remaining = abs(atoi(walk + 1));
         /* on some systems POWER_SUPPLY_POWER_NOW does not exist, but actually
          * it is the same as POWER_SUPPLY_CURRENT_NOW but with μWh as
          * unit instead of μAh. We will calculate it as we need it
